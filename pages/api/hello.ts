04a2d2680de953ebf0a3e51e5fb65dff709f1316
Richie Bendall <richiebendall@gmail.com>
2021-04-10 20:37:09 +1200

    Properly type api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
-import { VercelRequest, VercelResponse } from "@vercel/node"
+import type { NextApiRequest, NextApiResponse } from "next"
 
-const hello = (_request: VercelRequest, response: VercelResponse) => {
+const hello = (_request: NextApiRequest, response: NextApiResponse) => {
 	response.statusCode = 200
 	response.json({ name: "John Doe" })
 }
