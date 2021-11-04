54f0908e8306d87a41e507a30701637c8dedef3b
Richie Bendall <richiebendall@gmail.com>
2021-11-05 03:35:24 +1300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 			<main className={styles.main}>
 				<h1 className={styles.title}>Welcome to <a href='https://nextjs.org'>Next.js!</a></h1>
 
-				<p className={styles.description}>Get started by editing<code className={styles.code}>pages/index.js</code></p>
+				<p className={styles.description}>Get started by editing<code className={styles.code}>pages/index.tsx</code></p>
 
 				<div className={styles.grid}>
 					<a href='https://nextjs.org/docs' className={styles.card}><h3>Documentation &rarr;</h3><p>Find in-depth information about Next.js features and API.</p></a>
